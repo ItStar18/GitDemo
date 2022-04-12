b3596c67fbe9651c7728a248adb5261ddf3cfe50
Star <itstar1809@163.com>
2022-04-12 22:27:04 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ZhQL
+ * @date 2022/4/12 22:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
